8c48d299fac9422ff52f45b900ece92d1e17ae3f
RobertoMartinR <robertomartinromoo@gmail.com>
2025-07-13 20:43:52 +0200

    Upload Project

---
+++ langchain_bot_using_messages.py
@@ -0,0 +1,35 @@
+import getpass
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage
+
+load_dotenv()
+
+if not os.environ.get("GOOGLE_API_KEY"):
+  os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
+os.environ["LANGSMITH_TRACING"] = "true"
+if "LANGSMITH_API_KEY" not in os.environ:
+    os.environ["LANGSMITH_API_KEY"] = getpass.getpass(
+        prompt="Enter your LangSmith API key (optional): "
+    )
+if "LANGSMITH_PROJECT" not in os.environ:
+    os.environ["LANGSMITH_PROJECT"] = getpass.getpass(
+        prompt='Enter your LangSmith Project Name (default = "default"): '
+    )
+    if not os.environ.get("LANGSMITH_PROJECT"):
+        os.environ["LANGSMITH_PROJECT"] = "default"
+
+model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+
+
+###########################################################################################################
+# Using Messages
+messages = [
+    SystemMessage("Translate the following from English into Italian"),
+    HumanMessage("hi!"),
+]
+
+model.invoke(messages)
+###########################################################################################################
